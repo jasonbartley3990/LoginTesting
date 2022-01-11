709248dd55abe0a6d186c5d43a0ad632e7eb9671
jasonbartley3990 <jkbartley25@yahoo.com>
2022-01-11 17:26:21 -0500

    Initial Commit

---
+++ LoginTesting/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LoginTesting
+//
+//  Created by Jason bartley on 1/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
